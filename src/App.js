505d7ff83e57bb5113b179c27b1a56744a872378
오승훈 <seunghun6257@gmail.com>
2024-02-24 05:12:52 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyWord, setKeyWord] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => {
+    setKeyWord(e.target.value);
+  };
 
-  console.log(
-    "아..... 그만.... 하고... 싶어..... 제...,.. 바라라라ㅏ라배랍라ㅑ밷ㄺㄷ8ㅑㅓ9ㅛㄷ모ㅔ랴9 ㅗ ㅔㄷㄱ려ㅑㅗ0 ㄷ로0ㅡㅓㅕ 뎌"
-  );
+  // 검색할때만 실행
+  useEffect(() => {
+    if (keyWord !== "") {
+      console.log("SEARCH FOR", keyWord);
+    }
+  }, [keyWord]);
 
-  useEffect(() => console.log("I Run Only Once!"), []);
+  // 버튼 눌룰때만 실행
+  useEffect(() => console.log("SEARCH FOR", counter), [counter]);
 
   return (
     <div className="App">
+      <input
+        value={keyWord}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text="Click me!" onClick={onClick} />
     </div>
