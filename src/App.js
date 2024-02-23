ada55bbe3bd177b7e6a08dd0098601e121b716d3
오승훈 <seunghun6257@gmail.com>
2024-02-24 04:48:54 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import styles from "./App.module.css";
 import Button from "./Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
+
+  console.log(
+    "아..... 그만.... 하고... 싶어..... 제...,.. 바라라라ㅏ라배랍라ㅑ밷ㄺㄷ8ㅑㅓ9ㅛㄷ모ㅔ랴9 ㅗ ㅔㄷㄱ려ㅑㅗ0 ㄷ로0ㅡㅓㅕ 뎌"
+  );
+
+  useEffect(() => console.log("I Run Only Once!"), []);
+
   return (
     <div className="App">
       <h1 className={styles.title}>{counter}</h1>
