7eb5c980e7f4b96a70045f36b5be92102b80749d
오승훈 <seunghun6257@gmail.com>
2024-02-26 00:45:31 +0900

    End

--- src/route/Detail.js
+++ src/route/Detail.js
@@ -9,17 +9,15 @@ function Detail() {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json.data.movie);
     setMovie(json.data.movie);
   };
 
   useEffect(() => {
     getMovie();
-  });
+  }, []);
 
   const [movie, setMovie] = useState([]);
   console.log(movie);
-  const MAP = movie.genres;
 
   return (
     <div>
@@ -27,9 +25,8 @@ function Detail() {
       <h2>{movie.title}</h2>
       <img src={movie.medium_cover_image}></img>
       <ul>
-        {MAP.map((value) => (
-          <li key={value}>{value}</li>
-        ))}
+        {movie.genres &&
+          movie.genres.map((value) => <li key={value}>{value}</li>)}
       </ul>
       <p>Run time: {movie.runtime} minutes</p>
       <p>{movie.summary === "" ? "정보가 없습니다." : movie.summary}</p>
